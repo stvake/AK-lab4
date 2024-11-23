49cc4efdaa6ad486129a78bb637a45ed4bf9887b
stvake <stvakemail@gmail.com>
2024-11-23 18:32:38 +0200

    Rewritten hello.c fixing weird formatting

--- hello.c
+++ hello.c
@@ -25,44 +25,29 @@ static LIST_HEAD(hello_list);
 static int __init hello_init(void)
 {
 	int i;
-	
-	if (hello_count == 0 || (hello_count >= 5 && hello_count <= 10)) 
-    {
-    	printk(KERN_WARNING "Warning: Invalid count (%u), is 0 or between 5 and 10\n", hello_count);
-    }
-    else if (hello_count > 10)
-    {
+
+	if (hello_count == 0 || (hello_count >= 5 && hello_count <= 10))
+	{
+		printk(KERN_WARNING "Warning: Invalid count (%u), is 0 or between 5 and 10\n", hello_count);
+	}
+	else if (hello_count > 10)
+	{
 		printk(KERN_ERR "Error: Count exceeds limit 10, exiting with -EINVAL\n");
 		return -EINVAL;
-    }
-
-    for (i = 0; i < hello_count; i++)
-    {
-    	struct hello_entry *entry = kmalloc(sizeof(*entry), GFP_KERNEL);
-    	if (!entry)
-        {
-    		printk(KERN_ERR "Error: Memory allocation failed\n");
-    		return -ENOMEM;
-    	}
-    	entry->time = ktime_get();
-    	list_add_tail(&entry->list, &hello_list);
-    	printk(KERN_EMERG "Hello, world!\n");
-    }
-
-    return 0;
-}
-
-static void __exit hello_exit(void)
-{
-	struct hello_entry *entry, *tmp;
-
-	list_for_each_entry_safe(entry, tmp, &hello_list, list)
-    {
-		printk(KERN_INFO "Time: %lld ns\n", ktime_to_ns(entry->time));
-		list_del(&entry->list);
-		kfree(entry);
 	}
-}
 
-module_init(hello_init);
-module_exit(hello_exit);
+	for (i = 0; i < hello_count; i++)
+	{
+		struct hello_entry* entry = kmalloc(sizeof(*entry), GFP_KERNEL);
+		if (!entry)
+		{
+			printk(KERN_ERR "Error: Memory allocation failed\n");
+			return -ENOMEM;
+		}
+		entry->time = ktime_get();
+		list_add_tail(&entry->list, &hello_list);
+		printk(KERN_EMERG "Hello, world!\n");
+	}
+
+	return 0;
+}
